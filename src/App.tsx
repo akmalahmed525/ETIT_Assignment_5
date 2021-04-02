8c9a4db99adbfc3bbf7976595338f2935c9979ed
Ahmed Akmal <akmal.ahmed525@gmail.com>
2021-04-02 21:10:45 +0530

    Changed from BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import SignUp from "src/forms/SignUp";
 import SignIn from "src/forms/SignIn";
